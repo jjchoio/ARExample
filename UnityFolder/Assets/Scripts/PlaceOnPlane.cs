f922ae04a20188c38501885aae9a0a4a72fa6439
Joshua Choi <im.joshuac@gmail.com>
2018-12-05 00:00:43 -0500

    AR Foundation with ARCore setup completed

---
+++ UnityFolder/Assets/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+
+/// <summary>
+/// modifed from the script from https://github.com/Unity-Technologies/arfoundation-samples
+/// </summary>
+
+[RequireComponent(typeof(ARSessionOrigin))]
+public class PlaceOnPlane : MonoBehaviour
+{
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedPrefab;
+    
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject { get; private set; }
+
+    ARSessionOrigin m_SessionOrigin;
+    
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+    
+    void Awake()
+    {
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (m_SessionOrigin.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = s_Hits[0].pose;
+
+                if (spawnedObject == null)
+                {
+                    spawnedObject = Instantiate(placedPrefab, hitPose.position, Quaternion.identity);
+                }
+                else
+                {
+                    spawnedObject.transform.position = hitPose.position;
+                }
+            }
+        }
+    }
+}
