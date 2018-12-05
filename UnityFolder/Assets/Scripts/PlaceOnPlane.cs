b7b8919856194dedd7b4af21b110b1f398fad185
Joshua Choi <im.joshuac@gmail.com>
2018-12-05 00:48:44 -0500

    basic interaction implemented

--- UnityFolder/Assets/Scripts/PlaceOnPlane.cs
+++ UnityFolder/Assets/Scripts/PlaceOnPlane.cs
@@ -15,40 +15,72 @@ public class PlaceOnPlane : MonoBehaviour
     /// The prefab to instantiate on touch.
     /// </summary>
     public GameObject placedPrefab;
-    
+    /// <summary>
+    /// Visualize where to instantiate the prefab
+    /// </summary>
+    public GameObject cursor;
+
     /// <summary>
     /// The object instantiated as a result of a successful raycast intersection with a plane.
     /// </summary>
     public GameObject spawnedObject { get; private set; }
 
-    ARSessionOrigin m_SessionOrigin;
+    private ARSessionOrigin m_SessionOrigin;
     
     static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
-    
+
+    private Camera ar_camera;
+    private Vector2 camera_center;
+
     void Awake()
     {
         m_SessionOrigin = GetComponent<ARSessionOrigin>();
     }
 
+    private void Start()
+    {
+        ar_camera = Camera.main;
+        camera_center = new Vector2(ar_camera.pixelWidth / 2, ar_camera.pixelHeight / 2);
+    }
+
     void Update()
     {
-        if (Input.touchCount > 0)
+
+        if (m_SessionOrigin.Raycast(camera_center, s_Hits, TrackableType.PlaneWithinPolygon))
         {
-            Touch touch = Input.GetTouch(0);
+            Pose hitPose = s_Hits[0].pose;
+            cursor.transform.position = hitPose.position;
 
-            if (m_SessionOrigin.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+            if (Input.touchCount > 0)
             {
-                Pose hitPose = s_Hits[0].pose;
-
+                // you can instantiate only once
                 if (spawnedObject == null)
                 {
                     spawnedObject = Instantiate(placedPrefab, hitPose.position, Quaternion.identity);
-                }
-                else
-                {
-                    spawnedObject.transform.position = hitPose.position;
+
+                    // turn off the cursor
+                    cursor.SetActive(false);
                 }
             }
         }
+
+        //if (Input.touchCount > 0)
+        //{
+        //    Touch touch = Input.GetTouch(0);
+
+        //    if (m_SessionOrigin.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+        //    {
+        //        Pose hitPose = s_Hits[0].pose;
+
+        //        if (spawnedObject == null)
+        //        {
+        //            spawnedObject = Instantiate(placedPrefab, hitPose.position, Quaternion.identity);
+        //        }
+        //        else
+        //        {
+        //            spawnedObject.transform.position = hitPose.position;
+        //        }
+        //    }
+        //}
     }
 }
