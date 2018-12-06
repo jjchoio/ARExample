91dec792f7e4cb6abc7467a723ac84cda7c213d3
Joshua Choi <im.joshuac@gmail.com>
2018-12-05 21:12:20 -0500

    simple feature for launching the rocket implemented

---
+++ UnityFolder/Assets/Scripts/ARGameController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Manager script
+/// </summary>
+
+namespace ARExample
+{
+
+    /// <summary>
+    /// singleton script for controlling Rocket model
+    /// </summary>
+   
+    public class ARGameController : MonoBehaviour
+    {
+
+        [HideInInspector]
+        public GameObject SpawnedRocket;
+
+        // Button to fire up the Rocket
+        public GameObject launchButton;
+        // joystick to control the direction of the Rocket
+        //public GameObject joystick;
+
+        // Private
+        private bool isLaunching;
+        private float firePower;
+
+        // Singleton
+        public static ARGameController Instance;
+        void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(this);
+        }
+
+        private void Start()
+        {
+            launchButton.SetActive(false);
+            firePower = 0; // rocket is not launching
+        }
+
+        /// <summary>
+        /// if a button is pressed, the rocket will gain power
+        /// if a button is released, the rocket will lose power
+        /// </summary>
+        void FixedUpdate()
+        {
+            if (SpawnedRocket)
+            {
+                if (isLaunching)
+                {
+                    firePower += 0.1f;
+                    if (firePower > 10)
+                        firePower = 10;
+                    ManipulateRocket();
+                }
+                else
+                {
+                    firePower -= 0.05f;
+                    if (firePower < 9) // always some power once launch starts
+                        firePower = 9;
+                    ManipulateRocket();
+                }
+            }
+        }
+
+        private void ManipulateRocket()
+        {
+            // for now, it is only moving upward
+            SpawnedRocket.GetComponent<Rigidbody>().AddForce(transform.up * firePower);
+
+            // NEXT STEP - implement joystick to control and move rocket around the scanned space
+        }
+
+        /// <summary>
+        /// gain power when the button is pressed
+        /// </summary>
+        public void PowerUp()
+        {
+            isLaunching = true;
+        }
+        /// <summary>
+        /// lose poewr when the button is released
+        /// </summary>
+        public void PowerDown()
+        {
+            isLaunching = false;
+        }
+
+        /// <summary>
+        /// show controller UI
+        /// </summary>
+        public void PrepareRocketControllerUI()
+        {
+            launchButton.SetActive(true);
+        }
+    }
+}
---
+++ UnityFolder/Assets/Scripts/SpawnPrefabModel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+
+/// <summary>
+/// Spawn prefab on a plane by tap on the screen
+/// initially learned from the script, https://github.com/Unity-Technologies/arfoundation-samples
+/// </summary>
+
+namespace ARExample
+{
+    [RequireComponent(typeof(ARSessionOrigin))]
+    public class SpawnPrefabModel : MonoBehaviour
+    {
+        // The prefab to instantiate on touch.
+        public GameObject rocketPrefab;
+
+        // Visualize where to instantiate the prefab
+        public GameObject spawnLocation;
+
+        // Private 
+        private bool isSpawned;
+        private ARSessionOrigin m_SessionOrigin;
+        static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+        private Camera ar_camera;
+        private Vector2 camera_center;
+
+        void Awake()
+        {
+            m_SessionOrigin = GetComponent<ARSessionOrigin>();
+        }
+
+        private void Start()
+        {
+            ar_camera = Camera.main;
+            camera_center = new Vector2(ar_camera.pixelWidth / 2, ar_camera.pixelHeight / 2);
+        }
+
+        void Update()
+        {
+            // always raycasting when prefab is NOT placed
+            // you can instantiate only once
+            if (isSpawned == false)
+            {
+                if (m_SessionOrigin.Raycast(camera_center, s_Hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = s_Hits[0].pose;
+
+                    // spawnLocation follows the center of camera
+                    spawnLocation.GetComponentInChildren<Renderer>().enabled = true;
+                    spawnLocation.transform.position = hitPose.position;
+                    spawnLocation.transform.rotation = hitPose.rotation;
+
+                    // touch to place the prefab & turn off the spawnLocation
+                    if (Input.touchCount > 0)
+                    {
+                        ARGameController.Instance.SpawnedRocket = Instantiate(rocketPrefab, hitPose.position, Quaternion.identity);
+                        ARGameController.Instance.PrepareRocketControllerUI();
+                        spawnLocation.GetComponentInChildren<Renderer>().enabled = false; // hide the spawnLocation object
+                        isSpawned = true;
+                    }
+                }
+                else
+                {
+                    spawnLocation.GetComponentInChildren<Renderer>().enabled = false;
+                }
+            }
+        }
+    }
+}
